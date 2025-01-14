264603442b86956784217b641c1d7d29c9cf2875
nxtcoder17 <nxtcoder17@gmail.com>
2025-01-14 13:14:03 +0530

    ci: adds a github action to test the flow

--- flake.nix
+++ flake.nix
@@ -22,6 +22,21 @@
           buildInputs = with pkgs; [
             go
             terraform
+
+            (stdenv.mkDerivation rec {
+              name = "run";
+              pname = "run";
+              src = fetchurl {
+                url = "https://github.com/nxtcoder17/Runfile/releases/download/v1.2.0/run-linux-amd64";
+                sha256 = "sha256-JbowwBx8BRWoyH8dMmD0PArYD6eKUVJXVQ4X3E9lXnY=";
+              };
+              unpackPhase = ":";
+              installPhase = ''
+                mkdir -p $out/bin
+                cp $src $out/bin/$name
+                chmod +x $out/bin/$name
+              '';
+            })
           ];
 
           shellHook = ''
